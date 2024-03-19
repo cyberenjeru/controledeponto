61987b437d22a1143ff42f7013e54927b98f02b5
cyberenjeru <viniciusbernardesxcx@gmail.com>
2024-03-18 22:13:41 -0300

    ocorrencia

---
+++ src/main/java/com/cyberenjeru/controledeponto/model/Occurrence.java
@@ -0,0 +1,14 @@
+package com.cyberenjeru.controledeponto.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Occurrence {
+
+}
