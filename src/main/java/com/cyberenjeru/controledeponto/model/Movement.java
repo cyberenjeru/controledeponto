2174fef877cbfa4386b62e8a032228cef765b42a
cyberenjeru <viniciusbernardesxcx@gmail.com>
2024-03-18 22:30:14 -0300

    classe e metodos de movimentação

--- src/main/java/com/cyberenjeru/controledeponto/model/Movement.java
+++ src/main/java/com/cyberenjeru/controledeponto/model/Movement.java
@@ -2,6 +2,11 @@ package com.cyberenjeru.controledeponto.model;
 
 import lombok.*;
 
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -9,4 +14,15 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class Movement {
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class MovementId implements Serializable {
+        private Long idMoviment;
+        private Long idUser;
+    }
+    @EmbeddedId
+    private MovementId id;
 }
